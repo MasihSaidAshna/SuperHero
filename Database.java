8eb1dfe705753aa8d635fb785c1e07a3eb3475f0
Masih <said0983@stud.kea.dk>
2022-09-12 18:38:33 +0200

    Superhero del 1 med user story 1 og 2 løst

--- Database.java
+++ Database.java
@@ -1,6 +1,6 @@
 public class Database {
-    private SuperHero[] helte = new SuperHero[5];
-    int n = 0;
+    private SuperHero[] helte = new SuperHero[5]; //Array med længde 5
+    int n = 0; //Counter
 
     public void addSuperHero (String supernavn, String superkraft, String oprindelsesår, int styrke) {
         SuperHero superhelt = new SuperHero(supernavn, superkraft, oprindelsesår, styrke);
--- Main.java
+++ Main.java
@@ -2,23 +2,35 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Indtast superheltenavn");
+        System.out.println("Velkommen til SUPERHERO Universet. \n1. Opret superhelt\n9. Afslut");
+        Scanner sc0 = new Scanner(System.in);
         Scanner sc1 = new Scanner(System.in);
-        String supernavn = sc1.next();
-        System.out.println("Indtast superkraft");
         Scanner sc2 = new Scanner(System.in);
-        String superkraft = sc2.next();
-        System.out.println("Indtast creation year");
         Scanner sc3 = new Scanner(System.in);
-        String cy = sc3.next();
-        System.out.println("Indtast styrke");
         Scanner sc4 = new Scanner(System.in);
-        int styrke = sc4.nextInt();
-
-
-        System.out.println("Supername: " + supernavn + " Superpower: " + superkraft + " Creation year: " + cy + " Strength: " + styrke);
-        Database superheltedata = new Database();
-        superheltedata.addSuperHero(supernavn,superkraft,cy,styrke);
-
+        int superMenu = sc0.nextInt();
+        for (int i = 0; i <= 5; i++) {
+            if (superMenu == 1) {
+                System.out.print("Indtast superheltens navn: "); 
+                String supernavn = sc1.next();
+                System.out.print("Indtast superkraft: ");
+                String superkraft = sc2.next();
+                System.out.print("Indtast oprindelsesår: ");
+                String creationYear = sc3.next();
+                System.out.print("Indtast styrke: ");
+                int styrke = sc4.nextInt();
+        
+                System.out.println("Supernavn: " + supernavn + "\nSuperkraft: " + superkraft + "\nOprindelsesår: " + creationYear + "\nStyrke: " + styrke);
+                Database superHeltData = new Database(); 
+                superHeltData.addSuperHero(supernavn, superkraft, creationYear, styrke);
+            }
+            else {break;}
+        }
+        sc0.close();
+        sc1.close();
+        sc2.close();
+        sc3.close();
+        sc4.close();
+        System.out.print("Tak for besøget!");
     }
 }
