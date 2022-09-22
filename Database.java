31c4383e660c92c049d830dd9f697142d34b7bc0
Masih <ashnacg@gmail.com>
2022-09-22 15:28:20 +0200

    Added update superhero functionality. Program now supports CRUD but lacks robustness and unit tests.

--- Database.java
+++ Database.java
@@ -4,6 +4,7 @@ import java.util.regex.Matcher;
 
 public class Database {
     public ArrayList<SuperHero> SuperListe = new ArrayList<SuperHero>();
+    private Scanner scDB = new Scanner(System.in);
 
     public void addSuperHero (String supernavn, String superkraft, int oprindelsesår, int styrke) {
         SuperHero superhelt = new SuperHero(supernavn, superkraft, oprindelsesår, styrke);
@@ -34,21 +35,56 @@ public class Database {
         }
     }
 
-/*    public void searchSuperHeroes(String supernavn) {
-        Pattern p = Pattern.compile(supernavn);
+    public void editSuperHero(String supernavn) {
+        int editChoice = -1; 
         if (SuperListe.isEmpty()) {
             System.out.println("List is empty!");
         }
-        for (SuperHero s : SuperListe){
-            Matcher m = p.matcher(s.getSuperName());
-            if (m.find()) {
-                System.out.println("Supername: " + s.getSuperName() + "\nSuperpower: " + s.getSuperPower() + "\nCreation year: " + s.getCreationYear() + "\nStrength: " + s.getStrength());
+        for (int i = 0; i < SuperListe.size(); i++){
+            SuperHero s = SuperListe.get(i);
+            if (s.getSuperName().equals(supernavn)) {
+
+                System.out.println("""
+                    ____________________
+                    Select which attribute you'd like to change for your superhero. 
+                    1. Edit superhero name 
+                    2. Edit super power
+                    3. Edit creation year
+                    4. Edit strength
+                    ____________________""");
+                    editChoice = scDB.nextInt();
+                    scDB.nextLine();
+
+                if (editChoice == 1) {
+                    System.out.print("Enter the superhero's new name: "); 
+                    String supername = scDB.next();
+                    scDB.nextLine();
+                    s.setSuperName(supername);
+                }
+                if (editChoice == 2) {
+                    System.out.print("Enter the superhero's new power: "); 
+                    String superpower = scDB.next();
+                    scDB.nextLine();
+                    s.setSuperPower(superpower);
+                }
+                if (editChoice == 3) {
+                    System.out.print("Enter the superhero's new creation year: "); 
+                    int creationYear = scDB.nextInt();
+                    scDB.nextLine();
+                    s.setCreationYear(creationYear);
+                }
+                if (editChoice == 4) {
+                    System.out.print("Enter the superhero's new strength: "); 
+                    int strength = scDB.nextInt();
+                    scDB.nextLine();
+                    s.setStrength(strength);
+                }
             }
             else {
                 System.out.println("Superhero not found");
             }
         }
-    }*/
+    }
 
     public void deleteSuperHero (String supernavn) {
         if (SuperListe.isEmpty()) {
--- UserInterface.java
+++ UserInterface.java
@@ -13,7 +13,8 @@ public class UserInterface {
                 1. Create superhero 
                 2. Show superhero list
                 3. Search superhero
-                4. Delete superhero
+                4. Edit superhero
+                5. Delete superhero
                 9. Terminate
                 ____________________""");
             userChoice = sc.nextInt();
@@ -26,23 +27,28 @@ public class UserInterface {
 
     public void menuOption(int userChoice) {
         if (userChoice == 1){
-            System.out.print("Enter the superhero's name: "); 
-            String superName = sc.next();
-            System.out.print("Enter superpower: ");
-            String superPower = sc.next();
-            System.out.print("Enter creation year: ");
-            String stringCreationYear = sc.next();
-            int creationYear = Integer.parseInt(stringCreationYear);
-            System.out.print("Enter strength: ");
-            String stringStrength = sc.next();
-            int strength = Integer.parseInt(stringStrength);
+            try {
+                System.out.print("Enter the superhero's name: "); 
+                String superName = sc.next();
+                System.out.print("Enter superpower: ");
+                String superPower = sc.next();
+                System.out.print("Enter creation year: ");
+                String stringCreationYear = sc.next();
+                int creationYear = Integer.parseInt(stringCreationYear);
+                System.out.print("Enter strength: ");
+                String stringStrength = sc.next();
+                int strength = Integer.parseInt(stringStrength);
 
-            db.addSuperHero(superName, superPower, creationYear, strength);
-            
-            System.out.println("""
-                ____________________
-                Superhero created. 
-                ____________________""");
+                db.addSuperHero(superName, superPower, creationYear, strength);
+                
+                System.out.println("""
+                    ____________________
+                    Superhero created. 
+                    ____________________""");
+            }
+            catch (Exception exception) {
+                System.out.println("ERROR: INVALID INPUT");
+            }
         }
         if (userChoice == 2) {
             db.showSuperHeroes();
@@ -54,11 +60,17 @@ public class UserInterface {
             db.searchSuperHeroes(superName);
         }
         if (userChoice == 4) {
+            System.out.println("Enter the superhero's name you want to edit: "); 
+            String superName = sc.next();
+            sc.nextLine();
+            db.editSuperHero(superName);
+            //db.scDB.close();
+        }
+        if (userChoice == 5) {
             System.out.println("Enter the superhero's name that you want to delete: "); 
             String superName = sc.next();
             sc.nextLine();
             db.deleteSuperHero(superName);
         }
     }
-
 }
